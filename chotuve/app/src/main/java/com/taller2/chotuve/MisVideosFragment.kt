cd5a856ff9461facbf0300c4b05d18e01cfede18
Franco Liberali <franco.liberali@gmail.com>
2020-05-22 17:30:17 -0300

    boton en mis videos que lleva a subir video

--- chotuve/app/src/main/java/com/taller2/chotuve/MisVideosFragment.kt
+++ chotuve/app/src/main/java/com/taller2/chotuve/MisVideosFragment.kt
@@ -1,14 +1,18 @@
 package com.taller2.chotuve
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class MisVideosFragment : Fragment() {
     companion object {
         fun newInstance(): MisVideosFragment = MisVideosFragment()
@@ -19,7 +23,22 @@ class MisVideosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis_videos, container, false)
+        val view = inflater.inflate(R.layout.fragment_mis_videos, container, false)  as View
+        val fab = view.findViewById(R.id.subir_video_boton) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            val intent = Intent()
+            intent.type = "video/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Seleccionar video"), 0)
+        }
+        return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            startActivity(Intent(activity, SubirVideoActivity::class.java))
+        }
     }
 
 }

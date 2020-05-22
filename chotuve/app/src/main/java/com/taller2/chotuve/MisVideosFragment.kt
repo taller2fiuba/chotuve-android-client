8d9af8a59d5c6b6887c6cfea5e960a608db7ffc4
Franco Liberali <franco.liberali@gmail.com>
2020-05-21 23:48:12 -0300

    videos e informacion en perfil

---
+++ chotuve/app/src/main/java/com/taller2/chotuve/MisVideosFragment.kt
@@ -0,0 +1,25 @@
+package com.taller2.chotuve
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MisVideosFragment : Fragment() {
+    companion object {
+        fun newInstance(): MisVideosFragment = MisVideosFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mis_videos, container, false)
+    }
+
+}
--- chotuve/app/src/main/java/com/taller2/chotuve/PerfilFragment.kt
+++ chotuve/app/src/main/java/com/taller2/chotuve/PerfilFragment.kt
@@ -1,14 +1,17 @@
 package com.taller2.chotuve
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class PerfilFragment : Fragment() {
     companion object {
         fun newInstance(): PerfilFragment = PerfilFragment()
@@ -19,7 +22,44 @@ class PerfilFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        val view = inflater.inflate(R.layout.fragment_perfil, container, false) as View
+
+        var pager = view.findViewById(R.id.perfil_view_pager) as ViewPager
+        setupViewPager(pager)
+
+        val toolbar = view.findViewById(R.id.perfil_toolbar) as TabLayout
+        toolbar.setupWithViewPager(pager)
+
+        return view
     }
 
+    private fun setupViewPager(viewPager : ViewPager) {
+        val viewPagerAdapter = ViewPagerAdapter(childFragmentManager)
+        viewPagerAdapter.addFragment(MisVideosFragment.newInstance(), "videos")
+        viewPagerAdapter.addFragment(MiInformacionFragment.newInstance(), "información")
+        viewPager.adapter = viewPagerAdapter
+    }
+
+    private class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        private val fragmentList = ArrayList<Fragment>()
+        private val fragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList.get(position)
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return fragmentTitleList[position]
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList.add(fragment)
+            fragmentTitleList.add(title)
+        }
+    }
 }

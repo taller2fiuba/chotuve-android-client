5e8f0df49ad04ac7e13e6a9ab6926e7edc236441
Lucho <sportelliluciano@gmail.com>
2020-06-19 02:28:44 -0300

    Agrego interacción con el servidor

--- chotuve/app/src/main/java/com/taller2/chotuve/presentador/PresentadorPerfil.kt
+++ chotuve/app/src/main/java/com/taller2/chotuve/presentador/PresentadorPerfil.kt
@@ -1,48 +1,39 @@
 package com.taller2.chotuve.presentador
 
 import android.os.Handler
+import android.util.Log
 import com.taller2.chotuve.modelo.Usuario
+import com.taller2.chotuve.modelo.interactor.InteractorPerfil
 import com.taller2.chotuve.vista.perfil.VistaInformacion
 
 
-class PresentadorPerfil (private val vista: VistaInformacion) {
-    fun onDestroy() {
+class PresentadorPerfil (private var vista: VistaInformacion?) {
+    private val interactor = InteractorPerfil()
 
+    fun onDestroy() {
+        vista = null
     }
 
     fun obtenerInformacion(usuarioId: Long?) {
-        val handler = Handler()
-        handler.postDelayed(
-            Runnable {
-                // mi perfil
-                if (usuarioId == null) {
-                    vista.mostrarPerfil(
-                        Usuario(
-                            1,
-                            "Lionel",
-                            "Messi",
-                            "franco.liberali@gmail.com",
-                            "1144162505",
-                            "Calle falsa 123",
-                            "https://www.mundodeportivo.com/r/GODO/MD/p5/MasQueDeporte/Imagenes/2018/10/24/Recortada/img_femartinez_20181010-125104_imagenes_md_otras_fuentes_captura-kcOG-U452531892714hYG-980x554@MundoDeportivo-Web.JPG"
-                        )
-                    )
-                } else {
-                    // perfil de usuarioId
-                    vista.mostrarPerfil(
-                        Usuario(
-                            2,
-                            "Ronaldinho",
-                            "Gaúcho",
-                            "franco.liberali@gmail.com",
-                            "1144162505",
-                            "Calle falsa 123",
-                            "https://tribunero.com/wp-content/uploads/2019/08/ronaldinhojpg.jpg"
-                        )
-                    )
-                }
-            },
-            1000
-        )
+        val callback = object : InteractorPerfil.CallbackCargarPerfil {
+            override fun onExito(usuario: Usuario) {
+                vista?.mostrarPerfil(usuario)
+            }
+
+            override fun onError() {
+                // ?
+                Log.d("P/Perfil", "Error genérico")
+            }
+
+            override fun onErrorRed() {
+                // ?
+                Log.d("P/Perfil", "Error de red")
+            }
+        }
+
+        if (usuarioId == null)
+            interactor.cargarMiPerfil(callback)
+        else
+            interactor.cargarPerfil(usuarioId, callback)
     }
 }

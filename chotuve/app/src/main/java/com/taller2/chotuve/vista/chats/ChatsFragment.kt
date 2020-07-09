d3579ddc4ccd9cc762cd1ebdaf0a6d1455411dc0
Franco Liberali <franco.liberali@gmail.com>
2020-07-09 20:33:35 -0300

    refactor fragments que usan rtdb

--- chotuve/app/src/main/java/com/taller2/chotuve/vista/chats/ChatsFragment.kt
+++ chotuve/app/src/main/java/com/taller2/chotuve/vista/chats/ChatsFragment.kt
@@ -6,86 +6,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.firebase.ui.database.SnapshotParser
 import com.google.firebase.database.*
 import com.taller2.chotuve.R
 import com.taller2.chotuve.modelo.Chat
-import com.taller2.chotuve.modelo.Modelo
 import com.taller2.chotuve.modelo.Usuario
 import com.taller2.chotuve.presentador.PresentadorContactos
-import com.taller2.chotuve.vista.componentes.ChatViewHolder
 import com.taller2.chotuve.vista.contactos.ContactosFragment
 import com.taller2.chotuve.vista.contactos.VistaContactos
+import com.taller2.chotuve.vista.scroll_infinito.ChatsAdapter
 import kotlinx.android.synthetic.main.fragment_chats.*
 
 
-class ChatsFragment : Fragment(), VistaContactos {
-    // TODO mucho codigo repetido con mensajes fragment y con videos adapter
-    private val miUsuarioId = Modelo.instance.id
-    private val RTDB_NODE = com.taller2.chotuve.BuildConfig.RTDB_NODE
-    private val RTDB_NODE_CHATS = com.taller2.chotuve.BuildConfig.RTDB_NODE_CHATS
-    private val CHATS_CHILD = "$RTDB_NODE/$RTDB_NODE_CHATS/$miUsuarioId"
-
+class ChatsFragment : FirebaseRTDBFragment(), VistaContactos {
     private val presentadorContactos = PresentadorContactos(this)
 
     private lateinit var contactos: Map<Long, Usuario>
-    private lateinit var firebaseDatabaseReference: DatabaseReference
-    private lateinit var firebaseAdapter: FirebaseRecyclerAdapter<Chat, ChatViewHolder>
-    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+        val view = inflater.inflate(R.layout.fragment_chats, container, false)
+        recyclerView = chats_recycler_view
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        linearLayoutManager = LinearLayoutManager(context)
         presentadorContactos.obtenerContactos()
     }
 
     override fun mostrarContactos(contactos: List<Usuario>) {
         this.contactos = contactos.map { it.id to it }.toMap()
-        firebaseDatabaseReference = FirebaseDatabase.getInstance().reference
         mostrarSinChatsSiAunNoHayChats()
         nuevo_chat_boton.setOnClickListener {
-            val newFragment = ContactosFragment(fragmentManager!!, false) { usuario: Usuario ->
-                fragmentManager!!.popBackStack()
-                val newFragment = MensajesFragment(generarKey(usuario.id), usuario)
-                val transaction = fragmentManager!!.beginTransaction().hide(this)
-                transaction.add(R.id.fragment_container, newFragment)
-                transaction.addToBackStack(null)
-                transaction.commit()
-            }
-            val transaction = fragmentManager!!.beginTransaction().hide(this)
-            transaction.add(R.id.fragment_container, newFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
+            irACrearChat()
         }
         configurarRecyclerView()
-        firebaseAdapter.startListening()
     }
 
     fun mostrarSinChatsSiAunNoHayChats() {
-        firebaseDatabaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {
-                Log.d("vista", error.message)
-            }
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (!snapshot.hasChild(CHATS_CHILD)) {
-                    mensaje_sin_chats.visibility = View.VISIBLE
-                    cargando_chats_barra_progreso.visibility = View.GONE
-                }
-            }
-        })
+        ejecutarAccionSiChildNoExiste(CHATS_CHILD) {
+            mensaje_sin_chats.visibility = View.VISIBLE
+            cargando_chats_barra_progreso.visibility = View.GONE
+        }
     }
 
     override fun setErrorRed() {
@@ -93,22 +61,11 @@ class ChatsFragment : Fragment(), VistaContactos {
         Toast.makeText(context, "Error del server", Toast.LENGTH_LONG).show()
     }
 
-    private fun generarKey(usuarioId: Long): String {
-        return if (usuarioId > miUsuarioId!!) {
-            "$miUsuarioId-$usuarioId"
-        } else {
-            "$usuarioId-$miUsuarioId"
-        }
-    }
-
-
     private fun configurarRecyclerView() {
-        chats_recycler_view.layoutManager = linearLayoutManager
-
         val chatsRef: DatabaseReference = firebaseDatabaseReference.child(CHATS_CHILD)
 
         val parser =
-            SnapshotParser<Chat> { dataSnapshot ->
+            SnapshotParser { dataSnapshot ->
                 val chat: Chat = dataSnapshot.getValue(Chat::class.java)!!
                 val destinatarioId = dataSnapshot.key!!.toLong()
                 chat.key = generarKey(destinatarioId)
@@ -120,57 +77,20 @@ class ChatsFragment : Fragment(), VistaContactos {
             FirebaseRecyclerOptions.Builder<Chat>()
                 .setQuery(chatsRef.orderByChild("orden"), parser)
                 .build()
-        firebaseAdapter =
-            object : FirebaseRecyclerAdapter<Chat, ChatViewHolder>(options) {
-                override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ChatViewHolder {
-                    val inflater = LayoutInflater.from(viewGroup.context)
-                    return ChatViewHolder(
-                        inflater.inflate(
-                            R.layout.componente_chat,
-                            viewGroup,
-                            false
-                        )
-                    )
-                }
-
-                override fun onBindViewHolder(
-                    viewHolder: ChatViewHolder,
-                    position: Int,
-                    chat: Chat
-                ) {
-                    mensaje_sin_chats.visibility = View.GONE
-                    cargando_chats_barra_progreso.visibility = View.GONE
-                    viewHolder.setChat(chat)
-                    viewHolder.clickListener = object : ChatViewHolder.Clicklistener {
-                        override fun onItemClick(view: View?, position: Int) {
-                            val chat = getItem(position)
-                            verMensajes(chat.key!!, chat.destinatario!!)
-                        }
-                    }
-                }
-            }
-
-        firebaseAdapter.registerAdapterDataObserver(object : AdapterDataObserver() {
-            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                super.onItemRangeInserted(positionStart, itemCount)
-                val mensajeCantidad: Int = firebaseAdapter.itemCount
-                val ultimoItemVisible: Int = linearLayoutManager.findLastCompletelyVisibleItemPosition()
-                // Si es la carga inicial del recycler o
-                // estoy parado en el fondo de la lista
-                // entonces ir al fondo de la lista y mostrar el nuevo mensaje
-                if (ultimoItemVisible == -1 ||
-                    positionStart >= mensajeCantidad - 1 &&
-                    ultimoItemVisible == positionStart - 1
-                ) {
-                    chats_recycler_view.scrollToPosition(positionStart)
-                }
-            }
-        })
-
-        chats_recycler_view.adapter = firebaseAdapter
+        firebaseAdapter = ChatsAdapter(this, options) as FirebaseRecyclerAdapter<Any?, RecyclerView.ViewHolder>
+        recyclerView.adapter = firebaseAdapter
+        firebaseAdapter.startListening()
     }
 
-    private fun verMensajes(key: String, destinario: Usuario) {
+    private fun generarKey(usuarioId: Long): String {
+        return if (usuarioId > miUsuarioId!!) {
+            "$miUsuarioId-$usuarioId"
+        } else {
+            "$usuarioId-$miUsuarioId"
+        }
+    }
+
+    fun verMensajes(key: String, destinario: Usuario) {
         val newFragment = MensajesFragment(key, destinario)
         val transaction = fragmentManager!!.beginTransaction().hide(this)
         transaction.add(R.id.fragment_container, newFragment)
@@ -178,25 +98,24 @@ class ChatsFragment : Fragment(), VistaContactos {
         transaction.commit()
     }
 
-    override fun onStart() {
-        super.onStart()
-        if (this::firebaseAdapter.isInitialized) {
-            firebaseAdapter.startListening()
+    private fun irACrearChat() {
+        val contactosFragment = ContactosFragment(fragmentManager!!, false) { usuario: Usuario ->
+            // cuando toco en un contacto ir a los mensajes con ese contacto
+            fragmentManager!!.popBackStack()
+            val mensajesFragment = MensajesFragment(generarKey(usuario.id), usuario)
+            val transaction = fragmentManager!!.beginTransaction().hide(this)
+            transaction.add(R.id.fragment_container, mensajesFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
         }
+        val transaction = fragmentManager!!.beginTransaction().hide(this)
+        transaction.add(R.id.fragment_container, contactosFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
-    override fun onPause() {
-        if (this::firebaseAdapter.isInitialized) {
-            firebaseAdapter.stopListening()
-        }
-        super.onPause()
+    fun chatsCargados() {
+        mensaje_sin_chats.visibility = View.GONE
+        cargando_chats_barra_progreso.visibility = View.GONE
     }
-
-    override fun onResume() {
-        super.onResume()
-        if (this::firebaseAdapter.isInitialized) {
-            firebaseAdapter.startListening()
-        }
-    }
-
 }

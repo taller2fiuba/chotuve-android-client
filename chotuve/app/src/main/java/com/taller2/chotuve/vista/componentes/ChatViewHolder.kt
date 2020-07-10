02b6b63bc53c16a4eb8cc18f55ba71f6413e108a
Franco Liberali <franco.liberali@gmail.com>
2020-07-10 16:06:22 -0300

    agregar foto de perfil a chats

--- chotuve/app/src/main/java/com/taller2/chotuve/vista/componentes/ChatViewHolder.kt
+++ chotuve/app/src/main/java/com/taller2/chotuve/vista/componentes/ChatViewHolder.kt
@@ -1,14 +1,17 @@
 package com.taller2.chotuve.vista.componentes
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.taller2.chotuve.R
 import com.taller2.chotuve.modelo.Chat
+import com.taller2.chotuve.util.cargarImagen
 
 
 class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private var usuario: TextView = itemView.findViewById<View>(R.id.chat_usuario) as TextView
+    private var usuarioFotoPerfil: ImageView = itemView.findViewById<View>(R.id.destinatario_foto_de_perfil) as ImageView
+    private var usuarioEmail: TextView = itemView.findViewById<View>(R.id.chat_usuario) as TextView
     private var ultimoMensaje: TextView = itemView.findViewById<View>(R.id.chat_ultimo_mensaje) as TextView
     private var fechaYHoraUltimoMensaje: TextView = itemView.findViewById<View>(R.id.fecha_y_hora_ultimo_mensaje) as TextView
     var clickListener: Clicklistener? = null
@@ -25,7 +28,8 @@ class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     }
 
     fun setChat(chat: Chat) {
-        usuario.text = chat.destinatario!!.email
+        usuarioEmail.text = chat.destinatario!!.email
+        cargarImagen(chat.destinatario!!, usuarioFotoPerfil, itemView)
         ultimoMensaje.text =  chat.ultimoMensaje
         fechaYHoraUltimoMensaje.text = chat.fechaYHora
     }
